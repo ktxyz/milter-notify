ee4064724e0118abed6ad29e77ac0e934ad684ef
Debian <debian@ag-systems.pl>
2020-10-05 12:19:15 +0000

    Scripts are working

---
+++ scripts/enable.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+sudo systemctl start milter_notify
+sudo systemctl enable milter_notify
--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,19 @@ sudo cp bin/milter-notify /usr/local
 sudo cp milter_notify.service /etc/systemd/system/milter_notify.service
 
 sudo mkdir /etc/milter-notify
-sudo chown postfix:postfix /etc/milter-notify
-sudo chmod 755 /etc/milter-notify
+sudo chown postfix:mail /etc/milter-notify
+sudo chmod a+r /etc/milter-notify
 
-sudo echo "SECRET_KEY" > /etc/milter-notify/ping_data
-sudo echo "0.0.0.0" > /etc/milter-notify/ping_url
+echo 'SECRET_KEY' | sudo tee -a /etc/milter-notify/ping_data
+echo '0.0.0.0' | sudo tee -a /etc/milter-notify/ping_url
 
 sudo chmod a+r /etc/milter-notify/ping_data
 sudo chmod a+r /etc/milter-notify/ping_url
 
-sudo chown postfix:postfix /etc/milter-notify/ping_data
-sudo chown postfix:postifx /etc/milter-notify/ping_url
+sudo chown postfix:mail /etc/milter-notify/ping_data
+sudo chown postfix:mail /etc/milter-notify/ping_url
+
+echo "Everything done. Dont forget to change files:"
+echo -e "\t/etc/milter-notify/ping_data"
+echo -e "\t/etc/milter-notify/ping_url"
+
